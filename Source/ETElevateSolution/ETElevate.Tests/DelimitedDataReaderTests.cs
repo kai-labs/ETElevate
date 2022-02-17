83784fa1eb93f13c18eaeaa911d2e4ffb791ede8
Michael Bledsoe <michael.bledsoe@gmail.com>
2022-02-17 17:44:10 -0500

    Messing around with CSV parsing

---
+++ Source/ETElevateSolution/ETElevate.Tests/DelimitedDataReaderTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ETElevate.Tests
+{
+    [TestFixture]
+    public class DelimitedDataReaderTests
+    {
+        [TestCase("Red,Green,Blue\r\n", "Red", "Green", "Blue")]        
+        [TestCase("Red,\"Green,Light\",Blue\r\n", "Red", "Green,Light", "Blue")]
+        [TestCase("Red,\"Green,\nLight\",Blue\r\n", "Red", "Green,\nLight", "Blue")]
+        [TestCase("Red,\"Green,\r\nLight\",Blue\r\n", "Red", "Green,\r\nLight", "Blue")]
+        [TestCase("\"Red\"\"Bright\"\"\",\"Green,\r\nLight\",Blue\r\n", "Red\"Bright\"", "Green,\r\nLight", "Blue")]
+        [TestCase("\"Elephant,\r\nour favorite animal\",Africa", "Elephant,\r\nour favorite animal", "Africa")]
+        public void CanReadCsvSingleLine(string line, params string[] expectedValues)
+        {
+            using (var memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(line)))
+            {
+                using (var reader = new DelimitedDataReader(',', memoryStream))
+                {
+                    var fields = reader.ReadLine();
+
+                    for (var i = 0; i < expectedValues.Length; i++)
+                    {
+                        Assert.AreEqual(expectedValues[i], fields[i]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/ETElevateSolution/ETElevate/DelimitedDataReader.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ETElevate
+{
+    public class DelimitedDataReader : IDisposable
+    {
+        private readonly char delimiter;
+        private readonly Stream stream;
+
+        private enum ReadState
+        {   
+            StartField,            
+            ReadBareField,
+            ReadQuotedField,
+            LineDone
+        }
+                
+        public DelimitedDataReader(char delimiter, Stream stream)
+        {
+            this.delimiter = delimiter;
+            this.stream = stream;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public IList<string> ReadLine()
+        {
+            var fields = new List<string>();
+            var currentField = new StringBuilder();
+            var pendingChars = new StringBuilder();            
+            var readState = ReadState.StartField;
+
+            while (readState != ReadState.LineDone)
+            {
+                var value = stream.ReadByte();
+
+                if (value == -1)
+                {
+                    fields.Add(currentField.ToString());
+                    readState = ReadState.LineDone;
+                }
+
+                switch (readState)
+                {
+                    case ReadState.StartField:
+                        if (value == '"')
+                        {                            
+                            readState = ReadState.ReadQuotedField;                            
+                        }
+                        else if (value == '\r' || value == '\n')
+                        {
+                            readState = ReadState.LineDone;
+                        }
+                        else
+                        {
+                            currentField.Append((char)value);                            
+                            readState = ReadState.ReadBareField;
+                        }
+                        break;
+
+                    case ReadState.ReadBareField:                        
+                        if (value == delimiter)
+                        {
+                            fields.Add(currentField.ToString());
+                            currentField.Clear();
+                            readState = ReadState.StartField;
+                        }
+                        else if (value == '\r')
+                        {
+                            // ignore this.
+                        }
+                        else if (value == '\n')
+                        {
+                            fields.Add(currentField.ToString());
+                            currentField.Clear();
+                            readState = ReadState.StartField;
+                        }
+                        else
+                        {
+                            currentField.Append((char)value);
+                        }
+                        
+                        break;
+
+                    case ReadState.ReadQuotedField:
+                        if (pendingChars.Length == 0)
+                        {
+                            if (value == '"')
+                            {
+                                pendingChars.Append((char)value);
+                            }
+                            else
+                            {
+                                currentField.Append((char)value);
+                            }
+                        }
+                        else
+                        {
+                            pendingChars.Append((char)value);
+                            var pendingString = pendingChars.ToString();
+
+                            if (pendingString == "\"\"")
+                            {
+                                currentField.Append('"');
+                                pendingChars.Clear();
+                            }
+                            else if (pendingString == "\"\r\n" || pendingString == "\"\n")
+                            {
+                                fields.Add(currentField.ToString());
+                                currentField.Clear();
+                                pendingChars.Clear();
+                                readState = ReadState.LineDone;
+                            }
+                            else if (pendingString == $"\"{delimiter}")
+                            {
+                                fields.Add(currentField.ToString());
+                                currentField.Clear();
+                                pendingChars.Clear();
+                                readState = ReadState.StartField;
+                            }
+                            else
+                            {
+                                throw new Exception("Invalid character string.  Expecting delimiter, quote, CRLF, or newline");
+                            }
+                        }
+                        
+                        break;
+                }
+            }
+
+            return fields;
+        }
+    }
+}
